af8d8d85cfc8f243a33a9bd49f87f25b5ca35cdb
ofase <hipwg1@g.shingu.ac.kr>
2023-03-20 15:15:40 +0900

    Node JS

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Hello world!');
+
+});
+
+app.listen(3030, ()=> {
+
+    console.log('server is running at 3030 port');
+})
+
